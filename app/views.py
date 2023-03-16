1fe6467f9e619450db898d7186c1c8bca00da347
kattavyshnavi123 <kattavyshnavi2000@gmail.com>
2023-03-16 19:12:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def bubu(request):
+    return render(request,'bubu.html')
+
+
+def peachu(request):
+    return render(request,'peachu.html')
+    
